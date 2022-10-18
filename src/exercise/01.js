5986e3777208ec085f32ad196595e06d3353df13
Michael Crowe <michael.crowe@timeshighereducation.com>
2022-10-18 16:41:34 +0100

    Exercise 1 extra credit 4

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,23 +5,43 @@ import * as React from 'react'
 
 // The 1st argument - current value
 // The 2nd argument - called the "action" (dispatch function of our state)
-const countReducer = (state, newState) => {
-  // Supporting both function and Object
-  return {
-    ...state,
-    ...(typeof newState === 'function' ? newState(state) : newState),
+// const countReducer = (state, newState) => {
+//   // Supporting both function and Object
+//   return {
+//     ...state,
+//     ...(typeof newState === 'function' ? newState(state) : newState),
+//   }
+// }
+
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {count: state.count + action.step}
+    }
+    case 'DECREMENT': {
+      return {count: state.count - action.step}
+    }
+    default: {
+      throw new Error(`Unsupported action type: ${action.type}`)
+    }
   }
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
+  const decrement = () => dispatch({type: 'DECREMENT', step})
 
-  return <button onClick={increment}>{count}</button>
+  return (
+    <>
+      <button onClick={increment}>Up</button>
+      <button>{count}</button>
+      <button onClick={decrement}>Down</button>
+    </>
+  )
 }
 
 function App() {
