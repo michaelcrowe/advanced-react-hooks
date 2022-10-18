acb94c84cc16784aef1153c7df684f932c8ac6c6
Michael Crowe <michael.crowe@timeshighereducation.com>
2022-10-18 16:33:25 +0100

    Exercise 1 extra credit 3

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -6,8 +6,11 @@ import * as React from 'react'
 // The 1st argument - current value
 // The 2nd argument - called the "action" (dispatch function of our state)
 const countReducer = (state, newState) => {
-  console.log({...state, ...newState})
-  return {...state, ...newState}
+  // Supporting both function and Object
+  return {
+    ...state,
+    ...(typeof newState === 'function' ? newState(state) : newState),
+  }
 }
 
 function Counter({initialCount = 0, step = 1}) {
@@ -15,7 +18,8 @@ function Counter({initialCount = 0, step = 1}) {
     count: initialCount,
   })
   const {count} = state
-  const increment = () => setState({count: count + step})
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
 
   return <button onClick={increment}>{count}</button>
 }
